3220eec612fa36763114f0ff13424c2602c3d09b
Ada <Ada@Ada-PC>
2016-07-30 13:37:35 +0300

    Tema pentru cursul nr. 2

---
+++ TrainingsJava/src/my/messages/MyMessages.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package my.messages;
+
+/**
+ *
+ * @author Ada
+ */
+public class MyMessages {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("This my first Java Program");
+        System.out.println("So, hello Java it is");
+        System.out.println("Java is Wora!");
+      
+    }
+    
+}
